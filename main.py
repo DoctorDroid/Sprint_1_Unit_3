4e6e6dba9ebfe06c760ccccd16733ef6c4e7ad15
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-09-15 12:03:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class test_class:
+
+    def print_hi(name):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint. the rest of this will be to test if my pep 8 sensors will go off for a lilne being too many characters
+
+
+
+    # Press the green button in the gutter to run the script.
+
+    if __name__ == '__main__':
+        print_hi('PyCharm')
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pythonProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Timothy Eakin',
+    author_email='',
+    description=''
+)
